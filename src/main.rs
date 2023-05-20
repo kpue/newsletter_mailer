76291812ba75b85f422dd123bb379b6f895eb546
kpue <105126682+kpue@users.noreply.github.com>
2023-05-20 17:16:33 -0500

    Replaced logging with tracing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-//! src/main.rs
-
-use env_logger::Env;
 use newsletter_mailer::configuration::get_configuration;
 use newsletter_mailer::startup::run;
+use newsletter_mailer::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("newsletter_mailer".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
