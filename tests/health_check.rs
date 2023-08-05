3c3b9ead530b389b1328e73286591db8bc8e1da3
kpue <105126682+kpue@users.noreply.github.com>
2023-08-05 17:21:42 -0500

    connect to dev database

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,7 +2,6 @@ use newsletter_mailer::configuration::{get_configuration, DatabaseSettings};
 use newsletter_mailer::startup::run;
 use newsletter_mailer::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -44,18 +43,17 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     connection
-        .execute(&*format!(r#"CREATE DATABASE "{}";"#, &config.database_name))
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
         .await
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
 
