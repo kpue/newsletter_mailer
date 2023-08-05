5a423542b689f9c9d9f564ae455dcf5ba033b22f
kpue <105126682+kpue@users.noreply.github.com>
2023-08-05 17:59:08 -0500

    update sqlx and tokio

--- src/configuration.rs
+++ src/configuration.rs
@@ -47,7 +47,7 @@ impl DatabaseSettings {
 
     pub fn with_db(&self) -> PgConnectOptions {
         let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        options = options.log_statements(tracing_log::log::LevelFilter::Trace);
         options
     }
 }
